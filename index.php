2459616b99c44bda53e753228c568475dfcff34e
LorenzoMoccia <moccialorenzo98@gmail.com>
2023-01-09 14:37:13 +0100

    Terminato tutte le milestone obbligatorie

--- index.php
+++ index.php
@@ -30,8 +30,12 @@
     <?php 
     $primoFilm = new Movie("Narcos", "Andrés Baiz");
     $secondoFilm = new Movie("Natale in India","Neri Parenti");
+    
+    echo $primoFilm->titolo, "<br>";
+    echo $primoFilm->regista, "<br>";
 
-    echo $primoFilm;
+    echo $secondoFilm->titolo, "<br>";
+    echo $secondoFilm->regista, "<br>";
     ?>
 </body>
 </html>
